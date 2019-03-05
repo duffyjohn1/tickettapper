085790ab2b067419721c5e4f197bb48084f2d04d
John Duffy <john.duffy34@mail.dcu.ie>
2019-03-05 19:08:32 +0000

    Updated buddy

--- lib/InHome/pay.dart
+++ lib/InHome/pay.dart
@@ -87,73 +87,69 @@ class _MyNFCState extends State<MyNFC> {
             child: new Center(
               child: ListView(
                 children: <Widget>[
-                  Row(
+                  new Stack(
+                    alignment: Alignment.center,
                     children: <Widget>[
-                      new Text(
-                        "Ticket Tapper",
-                        style: new TextStyle(
-                          fontSize: 30.0,
-                          ),
-                        textAlign: TextAlign.center,
+                      new Container(
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(50.0),
+                            color: Color(0xFF18D191)),
+                        child: new Icon(
+                          Icons.euro_symbol,
+                          color: Colors.black,
                         ),
+                      ),
+                      new Container(
+                        margin: new EdgeInsets.only(right: 250.0, top: 50.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(50.0),
+                            color: Color(0xFFFC6A7F)),
+                        child: new Icon(
+                          Icons.home,
+                          color: Colors.black,
+                        ),
+                      ),
+                      new Container(
+                        margin: new EdgeInsets.only(left: 250.0, top: 50.0),
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(50.0),
+                            color: Color(0xFFFFCE56)),
+                        child: new Icon(
+                          Icons.directions_bus,
+                          color: Colors.black,
+                        ),
+                      ),
                     ],
-                    ),
-            new SizedBox(
-              height: 15.0,
-            ),
-            new Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                new Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF18D191)),
-                  child: new Icon(
-                    Icons.local_offer,
-                    color: Colors.black,
                   ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(right: 50.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFC6A7F)),
-                  child: new Icon(
-                    Icons.home,
-                    color: Colors.black,
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0, bottom: 80.0),
+                        child: new Text(
+                          "Ticket Tapper",
+                          style: new TextStyle(fontSize: 30.0),
+                        ),
+                      )
+                    ],
                   ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 30.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFFCE56)),
-                  child: new Icon(
-                    Icons.directions_bus,
-                    color: Colors.black,
-                  ),
-                ),
-
-              ],
-            ),
-
                   new SizedBox(
-                    height: 200.0,
+                    height: 20.0,
                     ),
 
                   new RaisedButton(
 
                       child: Text('Start NFC'),
                       textColor: Colors.white,
-                      color: Colors.red,
+                      color: Colors.black,
                       shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(150.0)),
+                          borderRadius: new BorderRadius.circular(200.0)),
                       onPressed: () {
                         startNFC();
                         //sleep(const Duration(seconds:5));
